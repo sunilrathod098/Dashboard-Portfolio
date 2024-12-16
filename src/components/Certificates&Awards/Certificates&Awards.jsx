fe7c4dfafc616a250154f24734b30024e73d80e4
sunilrathod098 <sunilrajputhrathod@gmail.com>
2024-12-16 10:19:38 +0530

    `Refactored Certificates&Awards component with updated dummy data and descriptions`

--- src/components/Certificates&Awards/Certificates&Awards.jsx
+++ src/components/Certificates&Awards/Certificates&Awards.jsx
@@ -11,7 +11,6 @@ const CertificatesAndAwards = () => {
     setHoveredTitle(null);
   };
 
-  // Dummy data for certificates
   const certificates = [
     {
       id: 1,
@@ -78,14 +77,14 @@ const CertificatesAndAwards = () => {
     },
   ];
 
-  // Dummy data for awards
+  
   const awards = [
     {
       id: 1,
       title: "Educational Certificate (First Class) from JNTUH",
       description:
         "Awarded First Class degree certificate from JNTUH College for excellence in academics.",
-      imageUrl: "https://link-to-image.com/award4.jpg",
+      imageUrl: "/public/certificates/acadamiccerti.jpg",
     },
     {
       id: 2,
@@ -94,13 +93,13 @@ const CertificatesAndAwards = () => {
         "Won first place in a regional hackathon with a tech-driven solution.",
       imageUrl: "/public/certificates/hackatonconenct.jpg",
     },
-    {
-      id: 3,
-      title: "Certified DevOps Engineer",
-      description:
-        "Completed a comprehensive DevOps certification, mastering tools like Docker, Kubernetes, Jenkins, and CI/CD pipelines.",
-      imageUrl: "https://link-to-image.com/devops-certification.jpg",
-    },
+    // {
+    //   id: 3,
+    //   title: "Certified DevOps Engineer",
+    //   description:
+    //     "Completed a comprehensive DevOps certification, mastering tools like Docker, Kubernetes, Jenkins, and CI/CD pipelines.",
+    //   imageUrl: "https://link-to-image.com/devops-certification.jpg",
+    // },
     {
       id: 4,
       title: "Course Completion Certificate from AISHWI Technologies Pvt. Ltd",
@@ -120,7 +119,7 @@ const CertificatesAndAwards = () => {
       title: "National Level Football Championship",
       description:
         "Represented and competed in a national-level football championship, showcasing teamlead, teamwork and sportsmanship.",
-      imageUrl: "https://link-to-image.com/national-football.jpg",
+      imageUrl: "/public/certificates/nlfootbal.jpg",
     },
   ];
 
@@ -129,7 +128,14 @@ const CertificatesAndAwards = () => {
       <h2 className="text-3xl font-semibold mb-6 text-white">Certificates</h2>
       <p className="text-lg text-gray-300 mb-6">
         These are some of the key certifications I've earned throughout my
-        professional journey.
+        professional journey, The certifications showcase expertise in software
+        development, data analytics, and advanced technologies. Key achievements
+        include a Software Developer internship at AISHWI Technologies,
+        certifications in Data Analytics and Machine Learning from PrepInsta,
+        and completion of a MERN Stack Developer bootcamp. Additional highlights
+        include Python certification from E-Box, a DSA course from Smart
+        Interview, and Generative AI training on LinkedIn, reflecting diverse
+        technical skills and continuous learning.
       </p>
 
       {/* Certificates Layout */}
@@ -137,7 +143,9 @@ const CertificatesAndAwards = () => {
         {certificates.map((item) => (
           <div
             key={item.id}
-            className="relative bg-gray-900 rounded-lg p-6 shadow-lg transform transition-transform duration-300 ease-in-out"
+            className={`relative bg-gray-900 rounded-lg p-6 shadow-lg transform transition-transform duration-300 ease-in-out ${
+              hoveredTitle === item.title ? "scale-105 z-10" : "scale-100"
+            }`}
             onMouseEnter={() => handleMouseEnter(item.title)}
             onMouseLeave={handleMouseLeave}
           >
@@ -161,7 +169,13 @@ const CertificatesAndAwards = () => {
       {/* Awards Section */}
       <h2 className="text-3xl font-semibold mb-6 text-white mt-16">Awards</h2>
       <p className="text-lg text-gray-300 mb-6">
-        Here are some of the awards I've received during my career.
+        Here are some of the awards I've received during my career, The
+        achievements reflect excellence in academics, innovation, and teamwork.
+        Highlights include a First Class Certificate from JNTUH, a Hackathon
+        Winner award for tech innovation, and a Course Completion Certificate
+        from AISHWI Technologies. The National Level CIT Internship Test and
+        participation in the National Level Football Championship showcase
+        professional skills, leadership, and sportsmanship.
       </p>
 
       {/* Awards Layout */}
@@ -169,7 +183,9 @@ const CertificatesAndAwards = () => {
         {awards.map((item) => (
           <div
             key={item.id}
-            className="relative bg-gray-900 rounded-lg p-6 shadow-lg transform transition-transform duration-300 ease-in-out"
+            className={`relative bg-gray-900 rounded-lg p-6 shadow-lg transform transition-transform duration-300 ease-in-out ${
+              hoveredTitle == item.title ? "scale-105 z-10" : "scale-100"
+            }`}
             onMouseEnter={() => handleMouseEnter(item.title)}
             onMouseLeave={handleMouseLeave}
           >
